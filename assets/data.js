f8c6b4d803c778f21dac1a861706393723dd4795
Klaus Sagehorn <huntersagehorn@gmail.com>
2026-07-29 11:53:49 -0500

    initial project setup

---
+++ assets/data.js
@@ -0,0 +1,53 @@
+const data = [
+  {
+    id: 1,
+    name: 'Margaret Hamilton',
+    description:
+      'Margaret Hamilton is an American computer scientist credited with coining the term "software engineering".',
+    image: 'margaret-hamilton.jpg',
+    altText: 'Margaret Hamilton parsing algorithms in the Draper Laboratory.',
+    url: 'https://en.wikipedia.org/wiki/Margaret_Hamilton_(software_engineer)',
+  },
+  {
+    id: 2,
+    name: 'Grace Hopper',
+    description:
+      'American Grace Hopper was one of the first programmers for the Harvard Mark I computer.',
+    image: 'grace-hopper.jpg',
+    altText:
+      'Grace Hopper, American computer scientist and United States Navy rear admiral, posing in her military uniform.',
+    url: 'https://en.wikipedia.org/wiki/Grace_Hopper',
+  },
+  {
+    id: 3,
+    name: 'Katherine Johnson',
+    description:
+      'Katherine Johnson is a West Virginia mathematician who helped confirm the accuracy of computers used by NASA.',
+    image: 'katherine-johnson.jpg',
+    altText:
+      'NASA research mathematician Katherine Johnson photographed at her desk at Langley Research Center in 1966.',
+    url: 'https://en.wikipedia.org/wiki/Katherine_Johnson',
+  },
+  {
+    id: 4,
+    name: 'Ada Lovelace',
+    description:
+      "London-born Ada Lovelace is credited with being the world's first computer programmer.",
+    image: 'ada-lovepace.jpg',
+    altText:
+      'Painting of Ada Lovelace donning traditional royal garments from the early 1800s.',
+    url: 'https://en.wikipedia.org/wiki/Ada_Lovelace',
+  },
+  {
+    id: 5,
+    name: 'Carol Shaw',
+    description:
+      'Carol Shaw is one of the first woman game designers and programmers in the video game industry.',
+    image: 'carol-shaw.jpg',
+    altText:
+      "Carol Shaw holding the box for the Atari 2600 version of her hit video game 'River Raid' around the time of its release in 1982.",
+    url: 'https://en.wikipedia.org/wiki/Carol_Shaw',
+  },
+];
+
+export default data;
